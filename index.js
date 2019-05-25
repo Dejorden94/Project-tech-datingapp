5b6e88979b0798a59b45208faa57cee7f06f47a1
Dejorden94 <d.moerman@hotmail.nl>
2019-05-25 16:29:28 +0200

    Update dating app
    - Sitemap toegevoegt
    - node_modules toegevoegt aan gitignore

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+var name = "Dejorden";
 
 app
 	.use(express.static('static'))
 	.use(notFound)
+	.use(views)
 	.listen(3000)
-
-app.use('/static', express.static('public'));
+	
+app.use('/static', express.static('static'));
 
 function notFound(req, res) {
   res.status(404).render('not-found.ejs')
