6fdb369ef4b85d9f7fc80d11be677da0c685ac7f
Dejorden94 <d.moerman@hotmail.nl>
2019-05-27 14:36:41 +0200

    Update app
    - html omgezet naar ejs
    - map static opgeruimd

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
 var app = express();
-var name = "Dejorden";
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Set static Path
+app.use(express.static(path.join(__dirname,'static')));
+
+
+app.get('/', function(req, res){
+	res.render('index',{
+		name: 'Dejorden'
+	});
+});
 
 app
-	.use(express.static('static'))
-	.use(notFound)
+	.use(notFound);
 	//.use(views)
-	.listen(3000);
+	app.listen(3000, function(){
+		console.log('Server gestart op Port 3000..');
+	});
 	
-app.use('/static', express.static('static'));
-
 function notFound(req, res) {
   res.status(404).render('not-found.ejs');
 }
--- static/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/test', (req res) =>{
-	res.send('Test route');
-});
-
-module.exports = router;
