242523724422020cfcfc115bd33b8e903f5c89b0
Dejorden94 <d.moerman@hotmail.nl>
2019-06-03 20:45:14 +0200

    dating-app update
    - mongoose vervangen door mongojs
    - css update
    - error wordt op de juiste plaats wergegeven

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongojs = require('mongojs');
+var db = mongojs('datingApp', ['interessts']);
+//test array
+var interesst = [{
+	name: 'Golf'
+}];
+var app = express();
+/*var mongoose = require('mongoose');*/
+/*var Schema = mongoose.Schema;*/
 //source: https://www.youtube.com/watch?v=oT2HOw3fWp4
 
 //connect to mongoddb
-mongoose.connect('mongodb://localhost/datingApp');
+/*mongoose.connect('mongodb://localhost/datingApp');
 
 var db = mongoose.connection;
 
@@ -20,12 +27,10 @@ mongoose.connection.once('open',function(){
 .on('error', function(error){
 	console.log('Ah oh, something went wrong. Darn..', error);
 });
-
+*/
 /*db.on('error', console.error.blind(console, 'connection error'));
 db.once('open', function(){
 });*/
-
-var app = express();
 
 //Set templating engine to ejs
 app.set('view engine', 'ejs');
@@ -33,26 +38,29 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 //Set static Path
-app.use(express.static(path.join(__dirname,'static')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 //Global vars
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.locals.errors = null;
 	next();
 });
 // Express validator middleware
 app.use(expressValidator({
-	errorFormatter: function(param, msg, value){
+	errorFormatter: function (param, msg, value) {
 		var namespace = param.split('.'),
-		root = namespace.shift() , formParam = root;
+			root = namespace.shift(),
+			formParam = root;
 
-		while(namespace.length){
+		while (namespace.length) {
 			formParam += '[' + namespace.shift() + ']';
 		}
-		return{
+		return {
 			param: formParam,
 			msg: msg,
 			value: value
@@ -60,55 +68,58 @@ app.use(expressValidator({
 	}
 }));
 
-//test array
-/*var interessts = [
-	{
-		name: 'Golf'
-	}
-];*/
-
+/*
+//Source: https://mongoosejs.com/docs/api.html#model_Model.find
 //Define a schema
-var intersstsSchema = mongoose.Schema({
-	name:String
-});
+var intersstsSchema = new Schema({name:String},
+	{collection: 'interessts'});
 
 var interesst = mongoose.model('interessts', intersstsSchema);
 
-interesst.find({name: {$gte: 'Gamen'}});
+var allInteressts = interesst.findOne({type: 'name'});
+*/
+app.get('/', function (req, res) {
+	db.interessts.find(function (err, docs) {
+		res.render('index', {
+			interesst: docs
+		});
+	});
 
-app.get('/', function(req, res){
-	res.render('index',{
-		interesst:interesst
-    });	
 });
 
-app.post('/interesses/add', function(req, res){
-	
-	req.checkBody('interesse', 'Veld mag niet leeg zijn').notEmpty();
+app.post('/', function (req, res) {
+
+	req.checkBody('interesst', 'Laat je matches weten wat jou interesseert').notEmpty();
 
 	var errors = req.validationErrors();
 
-	if(errors){
-		res.render('index',{
-			interesst: interesst,
+	if (errors) {
+		db.interessts.find(function (err, docs) {
+		res.render('index', {
+			interesst: docs,
 			errors: errors
 		});
-	}else{
-		var newInterest = {
-			interesst: req.body.interessts
+	});
+
+	} else {
+		var newInteresst = {
+			name: req.body.interesst
 		};
-		console.log(newInterest);
-	}	
-	
+		db.interessts.insert(newInteresst, function (error, result) {
+			if (error) {
+				console.log(error);
+			}
+			res.redirect('/');
+		});
+	}
 });
 
-app
-	.use(notFound);
-	//.use(views)
-	app.listen(3000, function(){
-		console.log('Server gestart op Port 3000..');
-	});
-	
+app.use(notFound);
+
+app.listen(3000, function () {
+	console.log('Server gestart op Port 3000..');
+});
+
 function notFound(req, res) {
-  res.status(404).render('not-found.ejs');
+	res.status(404).render('not-found.ejs');
 }
