fd9784ba6805e3f62c2f81d442d6ca4516b62e37
Dejorden94 <d.moerman@hotmail.nl>
2019-06-24 20:09:38 +0200

    :sparkles:  added scroll bar to see where you are in the interest overview

---
+++ static/js/progressive.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
